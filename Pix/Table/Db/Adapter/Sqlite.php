973664bdcb67d0b4132b15d76a5e0261840596c7
Repeat Yang <repeat@pixnet.tw>
2014-05-13 16:01:28 +0800

    Style: retab

--- Pix/Table/Db/Adapter/Sqlite.php
+++ Pix/Table/Db/Adapter/Sqlite.php
@@ -50,11 +50,11 @@ class Pix_Table_Db_Adapter_Sqlite extends Pix_Table_Db_Adapter_SQL
             throw new Exception("SQL Error: ({$errorInfo[0]}:{$errorInfo[1]}) {$errorInfo[2]} (SQL: {$sql})");
         }
         $res = $statement->execute();
-	if (($t = Pix_Table::getLongQueryTime()) and ($delta = (microtime(true) - $starttime)) > $t) {
+        if (($t = Pix_Table::getLongQueryTime()) and ($delta = (microtime(true) - $starttime)) > $t) {
             Pix_Table::debug(sprintf("[%s]\t%s\t%40s", $this->_path, $delta, $sql));
-	}
+        }
 
-	if ($res === false) {
+        if ($res === false) {
             if ($errno = $this->_pdo->errorCode()) {
                 $errorInfo = $this->_pdo->errorInfo();
             }
@@ -64,7 +64,7 @@ class Pix_Table_Db_Adapter_Sqlite extends Pix_Table_Db_Adapter_SQL
                 throw new Pix_Table_DuplicateException();
             }
             throw new Exception("SQL Error: ({$errorInfo[0]}:{$errorInfo[1]}) {$errorInfo[2]} (SQL: {$sql})");
-	}
+        }
         
         return new Pix_Table_Db_Adapter_PDO_Result($statement);
     }
@@ -83,13 +83,13 @@ class Pix_Table_Db_Adapter_Sqlite extends Pix_Table_Db_Adapter_SQL
         $primarys = is_array($table->_primary) ? $table->_primary : array($table->_primary);
         $pk_isseted = false;
 
-	foreach ($table->_columns as $name => $column) {
+        foreach ($table->_columns as $name => $column) {
             $s = $this->column_quote($name) . ' ';
             $db_type = in_array($column['type'], $types) ? $column['type'] : 'text';
 
-	    if ($column['unsigned'] and !$column['auto_increment']) {
-		$s .= 'UNSIGNED ';
-	    }
+            if ($column['unsigned'] and !$column['auto_increment']) {
+                $s .= 'UNSIGNED ';
+            }
 
             if ('int' == $db_type or $column['auto_increment']) {
                 $s .= 'INTEGER';
@@ -97,18 +97,18 @@ class Pix_Table_Db_Adapter_Sqlite extends Pix_Table_Db_Adapter_SQL
                 $s .= strtoupper($db_type);
             }
 
-	    if (in_array($db_type, array('varchar', 'char', 'binary'))) {
-		if (!$column['size']) {
-		    throw new Exception('you should set the option `size`');
-		}
-		$s .= '(' . $column['size'] . ')';
-	    }
+            if (in_array($db_type, array('varchar', 'char', 'binary'))) {
+                if (!$column['size']) {
+                    throw new Exception('you should set the option `size`');
+                }
+                $s .= '(' . $column['size'] . ')';
+            }
 
             // 在 varchar, char, text 加上 COLLATE NOCASE, 比較時忽略大小寫
             if (in_array($db_type, array('varchar', 'char', 'text'))) {
                 $s .= ' COLLATE NOCASE';
             }
-	    $s .= ' ';
+            $s .= ' ';
 
             if ($column['auto_increment']) {
                 if ($primarys[0] != $name or count($primarys) > 1) {
@@ -116,14 +116,14 @@ class Pix_Table_Db_Adapter_Sqlite extends Pix_Table_Db_Adapter_SQL
                 }
                 $s .= ' PRIMARY KEY AUTOINCREMENT ';
                 $pk_isseted = true;
-	    }
+            }
 
-	    if (isset($column['default']) and !$column['auto_increment']) {
+            if (isset($column['default']) and !$column['auto_increment']) {
                 $s .= 'DEFAULT ' . $this->quoteWithColumn($table, $column['default'], $name) . ' ';
-	    }
+            }
 
-	    $column_sql[] = $s;
-	}
+            $column_sql[] = $s;
+        }
 
         if (!$pk_isseted) {
             $s = 'PRIMARY KEY ' ;
@@ -135,7 +135,7 @@ class Pix_Table_Db_Adapter_Sqlite extends Pix_Table_Db_Adapter_SQL
             $column_sql[] = $s;
         }
 
-	$sql .= " (\n" . implode(", \n", $column_sql) . ") \n";
+        $sql .= " (\n" . implode(", \n", $column_sql) . ") \n";
 
         // CREATE TABLE
         $this->query($sql);
@@ -165,7 +165,7 @@ class Pix_Table_Db_Adapter_Sqlite extends Pix_Table_Db_Adapter_SQL
             throw new Pix_Table_Exception("要 DROP TABLE 前請加上 Pix_Setting::set('Table:DropTableEnable', true);");
         }
         $sql = "DROP TABLE " . $this->column_quote($table->getTableName());
-	return $this->query($sql, $table);
+        return $this->query($sql, $table);
     }
 
     /**
@@ -182,15 +182,15 @@ class Pix_Table_Db_Adapter_Sqlite extends Pix_Table_Db_Adapter_SQL
 
     public function quoteWithColumn($table, $value, $column_name = null)
     {
-	if (is_null($column_name)) {
+        if (is_null($column_name)) {
             return $this->_pdo->quote($value);
-	}
-	if ($table->isNumbericColumn($column_name)) {
-	    return intval($value);
-	}
-	if (!is_scalar($value)) {
+        }
+        if ($table->isNumbericColumn($column_name)) {
+            return intval($value);
+        }
+        if (!is_scalar($value)) {
             trigger_error("{$_SERVER['SERVER_NAME']}{$_SERVER['REQUEST_URI']} 的 column `{$column_name}` 格式不正確: " . gettype($value), E_USER_WARNING);
-	}
+        }
         return $this->_pdo->quote($value);
     }
 
