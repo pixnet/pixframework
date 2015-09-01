748e8e551b4734bd230bc85d4e618a12dbf3d347
Webber Wu <webber@pixnet.tw>
2015-09-01 10:21:44 +0800

    add Pix/DbConnectErrorException.php

---
+++ Pix/DbConnectErrorException.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Pix_Exception
+ *
+ * @uses Exception
+ * @copyright 2003-2012 PIXNET Digital Media Corporation
+ * @license http://framework.pixnet.net/license BSD License
+ */
+class Pix_DbConnectErrorException extends Exception
+{
+}
